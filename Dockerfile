a78adb5da754f20eec3232dfc8dfb3020e03972a
yrien30 <goran.yri@vendanor.com>
2020-10-14 15:16:36 +0200

    ..

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ ARG BUILD_FROM
 FROM mcr.microsoft.com/dotnet/core/aspnet:$BUILD_FROM
 WORKDIR /deploy
 COPY deploy/. ./
-ENTRYPOINT ["dotnet", "ConsoleApp1.dll"]
+ENTRYPOINT ["dotnet", "dockertest2.dll"]
--- Program.cs
+++ Program.cs
@@ -1,18 +1,26 @@
-﻿using System;
-using System.Threading;
-namespace ConsoleApp1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        { 
-            while(true)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-            {
-                Console.WriteLine("Hello World!");
-                Thread.Sleep(1000);
-            }
-            
+namespace dockertest2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
